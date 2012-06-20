fd291d68ef831d80a8a34641da382c8fb1f8df49
Stephen Chen <stephenchen0@gmail.com>
2012-06-20 18:42:17 -0400

    File & Basic Objects Defined

---
+++ calc.scala
@@ -0,0 +1,25 @@
+object HelloWorld {
+	def main(args: Array[String]) {
+		println("Hello, world!")
+	}
+}
+
+object Operation {
+
+}
+
+object Addition extends Operation {
+
+}
+
+object Subtraction extends Operation {
+	
+}
+
+object Multiplication extends Operation {
+	
+}
+
+object Division extends Operation {
+	
+}
