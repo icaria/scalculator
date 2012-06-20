08a70311a6bf1869cc6f45643ea4f26321e6d0eb
Stephen Chen <stephenchen0@gmail.com>
2012-06-20 19:34:26 -0400

    la

--- calc.scala
+++ calc.scala
@@ -7,57 +7,10 @@ package Calculator {
 		}
 	}
 
-	class Expression {
-		var exp
-		Expression 	
-	}
-
-
-	
-	abstract class Operation {
-		def compute(x: Int, y: Int)
+	abstract class Expression {
+		case class Number(value: Int) extends Expression
+		case class UnaryOp(operator: String, arg: Expression) extends Expression
+		case class BinaryOp(operator: String, left: Expression, right: Expression) extends Expression
 	}
 	
-	class Addition extends Operation {
-		def Addition() {
-		}
-
-		def compute(x: Int, y: Int) {
-			return x + y
-		}
-	}
-	
-	class Subtraction extends Operation {
-		def Subtraction() {
-			
-		}
-
-		def compute(x: Int, y: Int) {
-			return x - y
-		}
-	}
-	
-	class Multiplication extends Operation {
-		def Multiplication() {
-			
-		}
-
-		def compute(x: Int, y: Int) {
-			return x * y
-		}
-	}
-	
-	class Division extends Operation {
-		def Division() {
-			
-		}
-
-		def compute(x: Int, y: Int) {
-			if(y == 0) {
-				println("Nuclear Launch (Division by zero) detected.")
-				return -1
-			}
-			return x / y
-		}
-	}
 }
