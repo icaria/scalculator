4755fe59495fcd7639af4391a2afc6fad6e662b8
Stephen Chen <stephenchen0@gmail.com>
2012-06-20 19:21:36 -0400

    Hi JT.

--- calc.scala
+++ calc.scala
@@ -1,33 +1,39 @@
 package Calculator {
-	var exp = "4+6="
+	//var exp = "4+6="
 
 	object Calculator {
 		def main(args: Array[String]) {
 			println("This is a calculator")
 		}
 	}
+
+	class Expression {
+		var exp
+		Expression 	
+	}
+
+
 	
 	abstract class Operation {
-		def Operation(first: Int, second: Int)
-	}
-	
-	class ParseExpression {
-		
+		def compute(x: Int, y: Int)
 	}
 	
 	class Addition extends Operation {
-		var x;
-		var y;
 		def Addition() {
-			
+		}
+
+		def compute(x: Int, y: Int) {
+			return x + y
 		}
 	}
 	
 	class Subtraction extends Operation {
-		var x;
-		var y;
 		def Subtraction() {
 			
+		}
+
+		def compute(x: Int, y: Int) {
+			return x - y
 		}
 	}
 	
@@ -35,11 +41,23 @@ package Calculator {
 		def Multiplication() {
 			
 		}
+
+		def compute(x: Int, y: Int) {
+			return x * y
+		}
 	}
 	
 	class Division extends Operation {
 		def Division() {
 			
+		}
+
+		def compute(x: Int, y: Int) {
+			if(y == 0) {
+				println("Nuclear Launch (Division by zero) detected.")
+				return -1
+			}
+			return x / y
 		}
 	}
 }
